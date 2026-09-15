368a7667212f481848715c9bd55fd4105111e226
Sam <samdelhaye06@gmail.com>
2026-09-15 11:04:46 +0200

    sprint 2

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from player_class import player
 import json
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Player(BaseModel):
     id: int
@@ -33,7 +42,7 @@ def get_player(id: str):
         data = json.load(file)
 
     for player in data['players']:
-        if player['id'] == id:
+        if player['id'] == int(id):
             return player
 
     raise HTTPException(status_code=404, detail="Player not found")
